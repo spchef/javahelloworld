56a623b54fcd30e38452c9c2cfb290af60e535a3
root <root@mo-1a0164b1a.mo.sap.corp>
2016-05-18 09:41:37 +0000

    Changed by Sruni

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,8 +2,8 @@ public class JavaHelloWorld
 {
 	public static void main (String [] args)
 	{
-		System.out.println("Java Hello World ");
-		System.out.println("Hello Again");
+		System.out.println("Docker testing in Auto Build ");
+		System.out.println("Sruniraj Edited this File for testing at 3.01 PM ");
 	}
 }
 

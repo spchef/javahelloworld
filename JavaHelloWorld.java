65c248440d65bf9a766a7ca9436dd37343373dc1
root <root@mo-1a0164b1a.mo.sap.corp>
2016-05-24 06:36:14 +0000

    test

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,8 @@ public class JavaHelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("Docker testing in Auto Build ");
+
+   		System.out.println("Sruniraj Edited this File for testing at 3.01 PM ");
 		System.out.println("Sruniraj Edited this File for testing at 3.01 PM ");
 	}
 }
